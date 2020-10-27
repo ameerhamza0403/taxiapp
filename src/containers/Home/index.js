bd697920572cc44924a3ad5e6f6f138c3bd3667d
SyedAli00896 <SyedAli00896@gmail.com>
2020-10-27 12:16:11 +0500

    map container at home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,36 +1,36 @@
 import * as React from 'react';
 import {StyleSheet, Image, View} from 'react-native';
 import {Icon} from 'react-native-elements';
-import {NavigationEvents} from 'react-navigation';
 import {Card, Header, HorizontalRow} from '../../components';
 import {AppText, Button, Screen} from '../../components/common';
 
 export default function Home(props) {
   const {navigation} = props;
   return (
-    <Screen>
-      <View key="header">
+    <View style={styles.container}>
+      <View style={styles.map}></View>
+      <View style={styles.contentContainer}>
         <Header
           headerLeft={<Icon name="menu" type="material-icons" size={20} />}
           noTitle
           {...props}
         />
-      </View>
-      <View key="content">
-        <Card style={styles.card}>
-          <View style={styles.imageContainer}>
-            <Image source={require('../../assets/images/ic_route.png')} />
-          </View>
-          <View style={styles.locationContainer}>
-            <AppText> 24, ocean Avenue </AppText>
-            <HorizontalRow />
-            <AppText onPress={() => navigation.navigate('Select address')} gray>
-              i 'm going to
-            </AppText>
-          </View>
-        </Card>
-      </View>
-      <View key="footer">
+        <View style={styles.content}>
+          <Card style={styles.card}>
+            <View style={styles.imageContainer}>
+              <Image source={require('../../assets/images/ic_route.png')} />
+            </View>
+            <View style={styles.locationContainer}>
+              <AppText> 24, ocean Avenue </AppText>
+              <HorizontalRow />
+              <AppText
+                onPress={() => navigation.navigate('Select address')}
+                gray>
+                i 'm going to
+              </AppText>
+            </View>
+          </Card>
+        </View>
         <Button onPress={() => navigation.navigate('Arriving')}>
           Arriving
         </Button>
@@ -38,14 +38,26 @@ export default function Home(props) {
         <Button onPress={() => navigation.navigate('Ride details')}>
           Ride details
         </Button>
-        <Button onPress={() => navigation.navigate('Driver details')}>
+        <Button
+          style={styles.button}
+          onPress={() => navigation.navigate('Driver details')}>
           Driver details
         </Button>
       </View>
-    </Screen>
+    </View>
   );
 }
 const styles = StyleSheet.create({
+  container: {flex: 1},
+  contentContainer: {flex: 1, paddingHorizontal: 20},
+  content: {flex: 1},
+
+  map: {
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'green',
+  },
   card: {
     flexDirection: 'row',
     paddingVertical: 15,
@@ -57,5 +69,8 @@ const styles = StyleSheet.create({
   },
   locationContainer: {
     width: '85%',
+  },
+  button: {
+    marginBottom: 20,
   },
 });
